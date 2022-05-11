4bf09a195afaad010846c7ce5a775a363be6977d
Vernarec Pavol <vernarec@cngroup.dk>
2022-05-11 13:44:37 +0200

    Header and Footer added to Layout

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,9 +1,13 @@
 import { Container } from "@mui/material";
+import { Footer } from "./Footer";
+import { Header } from "./Header";
 
 export function Layout({ children }) {
   return (
     <>
+      <Header />
       <Container>{children}</Container>
+      <Footer />
     </>
   );
 }
