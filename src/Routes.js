a67c9375df1d339058d0552fdf83812262b5770d
Vernarec Pavol <vernarec@cngroup.dk>
2022-05-11 10:17:16 +0200

    Created Homepage.js and added to Routes as index

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,10 @@
 import { Route, Routes as RouterRoutes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 
 export function Routes() {
     return (
-        <RouterRoutes></RouterRoutes>
+        <RouterRoutes>
+            <Route index element={<Homepage />} ></Route>
+        </RouterRoutes>
     )
 }
