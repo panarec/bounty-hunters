6c87f7d936e0b55db43d850f10f3bf6899f5b120
Vernarec Pavol <vernarec@cngroup.dk>
2022-05-12 11:31:51 +0200

    prettierrc.yaml .eslintrc.js created

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.export = {
+  "plugins": ["simple-import-sort", "import"],
+  "rules": {
+    "simple-import-sort/imports": [
+      "error",
+      {
+        "groups": [
+          // Side effect imports.
+          ["^\\u0000"],
+          // Packages.
+          // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+          ["^(react|react-dom)(/.*|$)", "^(next)(/.*|$)", "^@?\\w"],
+          // Internal packages.
+          ["^(src)(/.*|$)"],
+          // Absolute imports and other imports such as Vue-style `@/foo`.
+          // Anything not matched in another group.
+          ["^"],
+          // Parent imports. Put `..` last.
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          // Style imports.
+          ["^.+\\.s?css$"]
+        ]
+      }
+    ],
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error"
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import "./custom.scss";
-
 function App() {
   return <div className="App"></div>;
 }
