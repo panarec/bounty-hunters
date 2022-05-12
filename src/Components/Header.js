e22e8a865b96d54d62f967a9d2275167c709ef1f
Vernarec Pavol <vernarec@cngroup.dk>
2022-05-12 15:28:37 +0200

    Navbar included to Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,3 +1,12 @@
+import { Container } from '@mui/material';
+import { Link } from 'react-router-dom';
+
 export const Header = () => {
-  return <div></div>;
+  return (
+    <div className="header">
+      <Container>
+        <Navbar></Navbar>
+      </Container>
+    </div>
+  );
 };
