1b752e2fbb9aa579efae2af32eb1474c7669497c
Vernarec Pavol <vernarec@cngroup.dk>
2022-05-13 10:51:52 +0200

    Additional styling to menu btn

--- src/Components/MenuButton.js
+++ src/Components/MenuButton.js
@@ -1,9 +1,16 @@
 import MenuIcon from '@mui/icons-material/Menu';
+import { IconButton } from '@mui/material';
 
 export const MenuButton = () => {
   return (
-    <IconButton aria-label="menu">
-      <MenuIcon />
+    <IconButton
+      className="menu-btn"
+      size="large"
+      aria-label="menu"
+      color="inherit"
+      sx={{ position: 'absolute', zIndex: '9999' }}
+    >
+      <MenuIcon fontSize="large" />
     </IconButton>
   );
 };
