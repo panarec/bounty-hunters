c2aa1e281626b36feb944fff43cb6d52644694ab
Vernarec Pavol <vernarec@cngroup.dk>
2022-05-16 16:30:41 +0200

    Dynamic links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,25 +1,34 @@
 import { Link } from 'react-router-dom';
 
 export const Navbar = ({ sidebarActive }) => {
+  const links = [
+    {
+      route: '/',
+      name: 'HOME',
+    },
+    {
+      route: '/',
+      name: 'STATISTICS',
+    },
+    {
+      route: '/',
+      name: 'PRODUCTS',
+    },
+  ];
+
   return (
     <div className="navbar">
       <nav>
-        <ul className={`primary-nav ${sidebarActive && "active"}`}>
-          <li>
-            <Link className="nav-link" to="/">
-              HOME
-            </Link>
-          </li>
-          <li>
-            <Link className="nav-link" to="/">
-              STATISTICS
-            </Link>
-          </li>
-          <li>
-            <Link className="nav-link" to="/">
-              PRODUCTS
-            </Link>
-          </li>
+        <ul className={`primary-nav ${sidebarActive && 'active'}`}>
+          {links.map((link) => {
+            return (
+              <li>
+                <Link className="nav-link" to={link.route}>
+                  {link.name}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </div>
