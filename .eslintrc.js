3a8347d854426800326659b5aa5c83c105decb91
Vernarec Pavol <vernarec@cngroup.dk>
2022-05-16 09:51:15 +0200

    Finally ESlint fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,37 @@
 module.exports = {
-  "plugins": ["simple-import-sort", "import"],
-  "rules": {
-    "simple-import-sort/imports": [
-      "error",
+  plugins: ['simple-import-sort', 'import'],
+  extends: "react-app",
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module',
+  },
+  rules: {
+    'simple-import-sort/imports': [
+      'error',
       {
-        "groups": [
+        groups: [
           // Side effect imports.
-          ["^\\u0000"],
+          ['^\\u0000'],
           // Packages.
           // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
-          ["^(react|react-dom)(/.*|$)", "^(next)(/.*|$)", "^@?\\w"],
+          ['^(react|react-dom)(/.*|$)', '^(next)(/.*|$)', '^@?\\w'],
           // Internal packages.
-          ["^(src)(/.*|$)"],
+          ['^(src)(/.*|$)'],
           // Absolute imports and other imports such as Vue-style `@/foo`.
           // Anything not matched in another group.
-          ["^"],
+          ['^'],
           // Parent imports. Put `..` last.
-          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
           // Other relative imports. Put same-folder imports and `.` last.
-          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
           // Style imports.
-          ["^.+\\.s?css$"]
-        ]
-      }
+          ['^.+\\.s?css$'],
+        ],
+      },
     ],
-    "simple-import-sort/exports": "error",
-    "import/first": "error",
-    "import/newline-after-import": "error",
-    "import/no-duplicates": "error",
-    "extends": ["@salesforce/eslint-config-lwc/recommended"]
-  }
-}
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+  },
+};
