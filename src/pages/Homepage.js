621065c71a8be81364bde68b2d67b02cb5b3cd5c
Vernarec Pavol <vernarec@cngroup.dk>
2022-05-16 16:03:55 +0200

    Filters added

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,29 @@
-import { Grid, TextField } from '@mui/material';
+import { Grid, Slider, Stack, TextField } from '@mui/material';
 
+import { Filter } from '../Components/Filter';
 import { FiltersForm } from '../Components/FiltersForm';
 import { TextFieldFilter } from '../Components/TextFieldFilter';
 
 export const Homepage = () => {
-  return <FiltersForm heading="SEARCH"></FiltersForm>;
+  return (
+    <FiltersForm heading="SEARCH">
+      <Filter>
+        <TextFieldFilter label="Name" />
+      </Filter>
+      <Filter>
+        <TextFieldFilter label="Age" type="number" />
+      </Filter>
+      <Filter>
+        <TextFieldFilter label="Hair" select />
+      </Filter>
+      <Filter>
+        <TextFieldFilter label="Eyes" select />
+      </Filter>
+      <Filter>
+        <Stack spacing={2} direction="row">
+          <Slider aria-label='reward' />
+        </Stack>
+      </Filter>
+    </FiltersForm>
+  );
 };
