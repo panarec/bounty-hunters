83722f209c994a1f075a72299f12877a3f001a1c
Vernarec Pavol <vernarec@cngroup.dk>
2022-05-17 13:56:09 +0200

    Basic filter layout

--- src/Components/CriminalsSearch.js
+++ src/Components/CriminalsSearch.js
@@ -1,5 +1,6 @@
-
+import { CheckboxFilter } from './CheckboxFilter';
 import { Filter } from './Filter';
+import { SliderFilter } from './SliderFilter';
 import { TextFieldFilter } from './TextFieldFilter';
 
 export const CriminalsSearch = () => {
@@ -12,13 +13,22 @@ export const CriminalsSearch = () => {
         <TextFieldFilter label="Age" type="number" />
       </Filter>
       <Filter>
-        <TextFieldFilter label="Hair" select />
+        <TextFieldFilter label="Nationality" select />
       </Filter>
       <Filter>
         <TextFieldFilter label="Eyes" select />
       </Filter>
       <Filter>
-        
+        <SliderFilter aria-label="reward" title="Reward" min={0} max={100000} />
+      </Filter>
+      <Filter>
+        <CheckboxFilter label="Hair" />
+      </Filter>
+      <Filter>
+        <TextFieldFilter label="Race" select />
+      </Filter>
+      <Filter>
+        <CheckboxFilter label="Sex" />
       </Filter>
     </>
   );
--- src/Components/SliderFilter.js
+++ src/Components/SliderFilter.js
@@ -1,23 +1,33 @@
 import { Slider, Stack, Typography } from '@mui/material';
 
-export const SliderFilter = ({ min, max, ...rest }) => {
+export const SliderFilter = ({ min, max, title, ...rest }) => {
   return (
-    <Stack spacing={2} direction="row">
-      <Typography
-        variant="body1"
-        component="span"
-        sx={{ whiteSpace: 'nowrap' }}
-      >
-        {min}
-      </Typography>
-      <Slider {...rest} min={min} max={max} value={null} onChange={null} />
-      <Typography
-        variant="body1"
-        component="span"
-        sx={{ whiteSpace: 'nowrap' }}
-      >
-        {max}
-      </Typography>
-    </Stack>
+    <>
+      <Typography gutterBottom>{title}</Typography>
+      <Stack spacing={2} direction="row">
+        <Typography
+          variant="body1"
+          component="span"
+          sx={{ whiteSpace: 'nowrap' }}
+        >
+          {min}
+        </Typography>
+        <Slider
+          {...rest}
+          min={min}
+          max={max}
+          value={[0, 100000]}
+          onChange={null}
+          sx={{ color: '#fff' }}
+        />
+        <Typography
+          variant="body1"
+          component="span"
+          sx={{ whiteSpace: 'nowrap' }}
+        >
+          {max}
+        </Typography>
+      </Stack>
+    </>
   );
 };
