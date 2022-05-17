badb035d1e6a93d7800782521f0e2782acea43b9
Vernarec Pavol <vernarec@cngroup.dk>
2022-05-17 16:31:03 +0200

    Heading added to header image

--- src/Components/HeaderImage/HeaderImage.js
+++ src/Components/HeaderImage/HeaderImage.js
@@ -1,10 +1,13 @@
-import { saloonImg } from '../../assets/images';
+import { Typography } from '@mui/material';
 
 import './_headerImage.scss';
 
 export const HeaderImage = () => {
   return (
     <div className="header-image">
+      <Typography component="h1" variant="h1">
+        HELLO WORLD
+      </Typography>
     </div>
   );
 };
