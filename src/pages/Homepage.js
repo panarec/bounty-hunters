4312e21b09e35008a6912328bd21f2630dd904cb
Vernarec Pavol <vernarec@cngroup.dk>
2022-05-18 10:50:42 +0200

    Added to Homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,14 @@
-import { CardList } from "../Components/CardList";
+import { CardList } from '../Components/CardList';
+import { CriminalsSearch } from '../Components/CriminalsSearch';
+import { FiltersForm } from '../Components/FiltersForm';
 
 export const Homepage = () => {
-  return <div>
-    <CardList></CardList>
-  </div>
+  return (
+    <div>
+      <FiltersForm heading="SEARCH">
+        <CriminalsSearch></CriminalsSearch>
+      </FiltersForm>
+      <CardList></CardList>
+    </div>
+  );
 };
