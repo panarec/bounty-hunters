3aabc47373adfd2ddfa32ed666cfc48e71fb4416
Vernarec Pavol <vernarec@cngroup.dk>
2022-05-23 11:17:13 +0200

    Filters design changed

--- src/Components/TextFieldFilter.js
+++ src/Components/TextFieldFilter.js
@@ -1,16 +1,40 @@
 import { TextField } from '@mui/material';
-import { borderColor } from '@mui/system';
+import { borderColor, maxWidth } from '@mui/system';
 
 import { variables } from '../assets/variables';
 
 export const TextFieldFilter = ({ ...rest }) => {
-  const { whiteColor } = variables;
+  const { whiteColor, bgColor } = variables;
 
   return (
     <TextField
       {...rest}
       fullWidth
       variant="outlined"
+      sx={{
+        '& .MuiOutlinedInput-input': {
+          color: whiteColor,
+        },
+
+        '& .MuiOutlinedInput-notchedOutline': {
+          borderColor: whiteColor,
+          borderWidth: '2px',
+        },
+        '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':
+          {
+            borderColor: bgColor,
+            borderWidth: '4px',
+          },
+        '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
+          borderColor: bgColor,
+        },
+        '& .MuiFormLabel-root': {
+          color: whiteColor,
+        },
+        '& .MuiInputLabel-root.Mui-focused': {
+          color: bgColor,
+        },
+      }}
     />
   );
 };
