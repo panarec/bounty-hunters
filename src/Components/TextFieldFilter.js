ac2e986aee8f8df98239f41a5662b9cad7dbd1f0
Vernarec Pavol <vernarec@cngroup.dk>
2022-05-25 09:03:11 +0200

    Custom MUI created

--- src/Components/TextFieldFilter.js
+++ src/Components/TextFieldFilter.js
@@ -1,39 +1,15 @@
 import { TextField } from '@mui/material';
 
-import { variables } from '../assets/variables';
+import {WhiteTextField} from "./CustomMUI"
 
 export const TextFieldFilter = ({ ...rest }) => {
-  const { whiteColor, bgColor } = variables;
 
   return (
     <TextField
       {...rest}
       fullWidth
       variant="outlined"
-      sx={{
-        '& .MuiOutlinedInput-input': {
-          color: whiteColor,
-        },
-
-        '& .MuiOutlinedInput-notchedOutline': {
-          borderColor: whiteColor,
-          borderWidth: '2px',
-        },
-        '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':
-          {
-            borderColor: bgColor,
-            borderWidth: '4px',
-          },
-        '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
-          borderColor: bgColor,
-        },
-        '& .MuiFormLabel-root': {
-          color: whiteColor,
-        },
-        '& .MuiInputLabel-root.Mui-focused': {
-          color: bgColor,
-        },
-      }}
+      sx={WhiteTextField}
     />
   );
 };
