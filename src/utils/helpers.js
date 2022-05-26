0a89613631ee1c4c1871446b46ac9e78171511da
Vernarec Pavol <vernarec@cngroup.dk>
2022-05-26 13:15:06 +0200

    Formater helper edited

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,6 @@
 export const formatDetailsKey = (key) => {
-  let formatedKey = key.replace('_', ' ');
+  let formatedKey = key.split('_');
+  formatedKey = formatedKey.join(' ');
   formatedKey =
     formatedKey.substring(0, 1).toUpperCase() +
     formatedKey.substring(1, formatedKey.lenght);
